cf8884371236d79f5ebaaf3d9e5595dad45e5be5
PedroG659 <150958889+PedroG659@users.noreply.github.com>
2023-11-29 17:42:47 -0300

    Update Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { NavBar} from './components/NavBar'
+import { Banner } from './components/Banner'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-
   return (
-    <div>
-      
+    <div className='App'>
+      <NavBar />
+      <Banner />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
