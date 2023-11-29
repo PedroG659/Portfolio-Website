4ccfcf915bd1227f87a19ed6da3cedbfad0c4b98
PedroG659 <150958889+PedroG659@users.noreply.github.com>
2023-11-29 17:52:31 -0300

    Update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -48,7 +48,7 @@ export const NavBar = () => {
                 <a href="#"><img src={navIcon2} alt="" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
             </div>
-            <button className="vvd" onClick={() => console.log('connect')}><span>Vamos nos Conectar</span></button>
+            <button className="vvd" onClick={() => console.log('connect')}><span>Vamos nos conectar</span></button>
           </span>
         </Navbar.Collapse>
       </Container>
