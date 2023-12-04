2816bf34eb37899bacee68e19cc1482bfb748566
PedroG659 <150958889+PedroG659@users.noreply.github.com>
2023-12-04 16:50:18 -0300

    Update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -34,7 +34,7 @@ export const Skills = () => {
             <div className="row">
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
+                        <h2>Habilidades</h2>
                         <p>Lorem Ipsum é simplesmente um texto fictício da indústria de impressão e composição tipográfica.<br></br> Lorem Ipsum tem sido o texto fictício padrão da indústria.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
