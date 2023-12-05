a8ed85bfe3aef7ef5eb46c9108acea1d5f69d9d7
PedroG659 <150958889+PedroG659@users.noreply.github.com>
2023-12-05 17:29:33 -0300

    Update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,6 +10,7 @@ export const Contact = () => {
     phone: '',
     message: ''
   }
+  
   const [formDetails, setFormDetails] = useState(formInitialDetails);
   const [buttonText, setButtonText] = useState('Send');
   const [status, setStatus] = useState({});
@@ -53,7 +54,7 @@ export const Contact = () => {
           <Col size={12} md={6}>
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Get In Touch</h2>
+                <h2>Entrar em contato</h2>
                 <form onSubmit={handleSubmit}>
                   <Row>
                     <Col size={12} sm={6} className="px-1">
